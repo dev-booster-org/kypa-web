5e4d0342f4ee01f8b81895da24c07b0e167058cc
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2025-06-02 14:41:17 -0300

    feat: improvements on menu style

--- src/components/public-menu/index.tsx
+++ src/components/public-menu/index.tsx
@@ -6,13 +6,15 @@ export function PublicMenu() {
   const { i18n } = useTranslation()
 
   return (
-    <div className="flex items-center gap-2 p-1 border-b border-gray-200 w-full justify-end bg-gray-100">
-      <Button variant="ghost" onClick={() => i18n.changeLanguage('pt')}>
-        <Globe /> PT
-      </Button>
-      <Button variant="ghost" onClick={() => i18n.changeLanguage('en')}>
-        <Globe /> EN
-      </Button>
+    <div className="flex items-center gap-2 p-1 border-b border-gray-200 w-full justify-end bg-blue-50">
+      <div className="min-w-[1400px] mx-auto flex items-center justify-end gap-2">
+        <Button variant="ghost" onClick={() => i18n.changeLanguage('pt')}>
+          <Globe /> PT
+        </Button>
+        <Button variant="ghost" onClick={() => i18n.changeLanguage('en')}>
+          <Globe /> EN
+        </Button>
+      </div>
     </div>
   )
 }
